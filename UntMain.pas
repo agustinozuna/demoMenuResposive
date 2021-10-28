ea759431de96d0765a0fbeba3eb7b0d6584cf7f7
kalopy <carlosozuna49@gmail.com>
2021-10-27 23:49:15 -0300

    Actualizacion de menu
    Cambio de version del menu a componente TMultiView

--- UntMain.pas
+++ UntMain.pas
@@ -8,12 +8,27 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   FMX.ListView.Types, FMX.ListView.Appearances, FMX.ListView.Adapters.Base,
   FMX.Ani, FMX.ListView, FMX.Objects, FMX.Controls.Presentation, FMX.StdCtrls,
-  FMX.Layouts, FMX.Effects;
+  FMX.Layouts, FMX.Effects, FMX.MultiView;
 
 type
   TfrmMain = class(TForm)
+    layout_principal: TLayout;
+    Rectangle1: TRectangle;
+    Layout1: TLayout;
+    Label1: TLabel;
+    img_menu: TImage;
+    ShadowEffect2: TShadowEffect;
+    mtvMenu: TMultiView;
+    lytMenu: TLayout;
     rect_menu: TRectangle;
     AnimationMenu: TFloatAnimation;
+    Layout5: TLayout;
+    Layout2: TLayout;
+    Layout3: TLayout;
+    Path1: TPath;
+    Layout4: TLayout;
+    Label3: TLabel;
+    SpeedButton2: TSpeedButton;
     lytExit: TLayout;
     lytIcoExit: TLayout;
     pthIcoExit: TPath;
@@ -25,25 +40,8 @@ type
     SpeedButton1: TSpeedButton;
     Circle3: TCircle;
     Circle4: TCircle;
-    Layout2: TLayout;
-    Layout3: TLayout;
-    Path1: TPath;
-    Layout4: TLayout;
-    Label3: TLabel;
-    SpeedButton2: TSpeedButton;
-    layout_principal: TLayout;
-    Rectangle1: TRectangle;
-    Layout1: TLayout;
-    Label1: TLabel;
-    img_menu: TImage;
-    rect_backgroundOpacity: TRectangle;
-    Layout5: TLayout;
     ShadowEffect1: TShadowEffect;
-    ShadowEffect2: TShadowEffect;
     procedure FormResize(Sender: TObject);
-    procedure img_menuClick(Sender: TObject);
-    procedure AnimationMenuFinish(Sender: TObject);
-    procedure rect_backgroundOpacityClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -61,11 +59,7 @@ procedure Close_Menu();
 begin
   with frmMain do
   begin
-    AnimationMenu.Inverse := true;
-    AnimationMenu.Start;
-    rect_menu.AnimateFloat('Opacity', 0, 0.4);
-    rect_backgroundOpacity.Visible := false;
-    rect_menu.Tag := 0;
+    mtvMenu.Mode := TMultiViewMode.Drawer;
   end;
 end;
 
@@ -73,11 +67,7 @@ procedure Open_Menu();
 begin
   with frmMain do
   begin
-    rect_menu.Visible := true;
-    AnimationMenu.Inverse := false;
-    AnimationMenu.Start;
-    rect_menu.AnimateFloat('Opacity', 1, 0.4);
-    rect_menu.Tag := 1;
+    mtvMenu.Mode := TMultiViewMode.Panel;
   end;
 end;
 
@@ -96,53 +86,22 @@ begin
     begin
       if (larg_screen < 600) then
       begin
-        // Si la pantalla es menor a 600 se cerrara el menu
-        img_menu.Visible := true;
-        layout_principal.Align := TAlignLayout.Top;
-        rect_menu.Align := TAlignLayout.FitLeft;
+        // Si la pantalla es menor a 600 se cerrara el menu(cambia de modo a Drawer)
         Close_Menu;
       end;
+
       if (larg_screen >= 600) then
       begin
-        // Si la pantalla es mayor a 599 se abrira el menu
-        img_menu.Visible := false;
-        layout_principal.Align := TAlignLayout.Horizontal;
-        rect_menu.Align := TAlignLayout.Left;
+        // Si la pantalla es mayor a 599 se abrira el menu (cambia de modo a Panel)
         Open_Menu;
       end;
     end;
   end;
 end;
 
-procedure TfrmMain.AnimationMenuFinish(Sender: TObject);
-begin
-  if frmMain.width < 600 then
-    img_menu.AnimateFloat('Opacity', 1, 0.2)
-  else
-    img_menu.AnimateFloat('Opacity', 0, 0.2);
-end;
-
 procedure TfrmMain.FormResize(Sender: TObject);
 begin
   Menu();
-end;
-
-procedure TfrmMain.img_menuClick(Sender: TObject);
-begin
-  if frmMain.rect_menu.Tag = 0 then
-  begin
-    Open_Menu;
-    rect_backgroundOpacity.Visible := true;
-  end
-  else
-  begin
-    Close_Menu;
-  end;
-end;
-
-procedure TfrmMain.rect_backgroundOpacityClick(Sender: TObject);
-begin
- Close_Menu;
 end;
 
 end.
